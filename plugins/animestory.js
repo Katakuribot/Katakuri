e4bcac90899aa31c086a30c6cb651a30cfa15fd2
Katakuribot <159330746+Katakuribot@users.noreply.github.com>
2024-03-16 16:47:32 +0000

    Update animestory.js

--- plugins/animestory.js
+++ plugins/animestory.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 
-    await conn.sendMessage(m.chat, { video: { url: dir[Math.floor(Math.random() * dir.length)] }, caption: `instagram.com/noureddine_ouafy` }, { quoted: m })
+    await conn.sendMessage(m.chat, { video: { url: dir[Math.floor(Math.random() * dir.length)] }, caption: `instagram.com/ibrahim_scp_` }, { quoted: m })
 }
 
 handler.help = ['animestory']
